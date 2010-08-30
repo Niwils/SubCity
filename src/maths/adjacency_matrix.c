503d683dac56ecc7f497d4ca581179ed4ef30666
Niels Grataloup <davelister@davelister.net>
2010-08-30 17:53:48 +0200

    added some files for maths structure.

---
+++ src/maths/adjacency_matrix.c
@@ -0,0 +1,2 @@
+// This file is reserved for functions in order to create, resolve, and/or do specific operations on Adjacency Matrix
+// You can also develop here functions to resolve Laplacian matrix (e.g.: for transports flows).
